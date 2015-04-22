88c7075508ed102267ee8ad50576ab9fb256a302
Samy Pesse <samypesse@gmail.com>
2015-04-22 15:08:02 -0500

    Use spaces instead of tabs

--- test/parser.js
+++ test/parser.js
@@ -3,12 +3,12 @@ var parser = require('../lib/parser');
 
 describe('Messages Parser', function() {
 
-	it('should correctly parse a call action', function() {
-		var r = parser.parse("Call +140145170479 for me");
+    it('should correctly parse a call action', function() {
+        var r = parser.parse("Call +140145170479 for me");
 
-		expect(r).to.be.ok;
-		expect(r.type).to.equal("call");
-		expect(r.phone).to.equal("+140145170479");
-	});
+        expect(r).to.be.ok;
+        expect(r.type).to.equal("call");
+        expect(r.phone).to.equal("+140145170479");
+    });
 
 });
