25396be7a69bd0ec728d4f9c2df0c7cb3124e22f
Samy Pesse <samypesse@gmail.com>
2015-04-23 18:35:36 -0500

    Add basic ability to make call

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,71 @@
+
+function setupDialDialog() {
+    var connection;
+    var $dialog = $('#dialog-dial');
+    var $input = $dialog.find('.dial-input');
+    var $status = $dialog.find('dial-status');
+    var $doHangup = $dialog.find('.do-hangup');
+    var $doCall = $dialog.find('.do-call');
+
+    var toggleCallStatus = function(){
+        $doCall.toggle();
+        $doHangup.toggle();
+    }
+
+
+    $dialog.find('.dial-button').click(function(e) {
+        e.preventDefault();
+        var value = $(e.currentTarget).val();
+
+        if (connection) {
+            connection.sendDigits(value);
+        } else {
+            $input.val($dialInput.val()+value);
+        }
+    });
+
+    Twilio.Device.setup($dialog.find('input[name=token]').val());
+
+    $doCall.click(function() {
+        params = { "Digits" : $doCall.val()};
+        connection = Twilio.Device.connect(params);
+    });
+    $doHangup.click(function() {
+        Twilio.Device.disconnectAll();
+    });
+
+    Twilio.Device.ready(function (device) {
+        $status.text('Ready to start call');
+    });
+
+    Twilio.Device.incoming(function (conn) {
+        if (confirm('Accept incoming call from ' + conn.parameters.From + '?')) {
+            $dialog.addClass('active');
+            connection = conn;
+            conn.accept();
+        }
+    });
+
+    Twilio.Device.offline(function (device) {
+        $status.text('Offline');
+    });
+
+    Twilio.Device.error(function (error) {
+        $status.text(error);
+    });
+
+    Twilio.Device.connect(function (conn) {
+        $status.text("Successfully established call");
+        toggleCallStatus();
+    });
+
+    Twilio.Device.disconnect(function (conn) {
+        $status.text("Call ended");
+        toggleCallStatus();
+    });
+}
+
+
 $(document).ready(function() {
     // Click to toggle dialog
     $('*[data-dialog]').click(function(e) {
@@ -6,4 +74,7 @@ $(document).ready(function() {
 
         $('#dialog-'+dialog).toggleClass('active');
     });
+
+    // Call dialog
+    setupDialDialog();
 });
