0b74f772359cbd80b204e7527844b290049a6b49
Samy Pesse <samypesse@gmail.com>
2015-04-24 11:03:59 -0500

    Improve display of audioplayer for recordings

--- public/js/main.js
+++ public/js/main.js
@@ -75,15 +75,71 @@ function setupDialDialog() {
     toggleCallStatus();
 }
 
-
-$(document).ready(function() {
-    // Click to toggle dialog
+// Click to toggle dialog
+function setupDialogs() {
     $('*[data-dialog]').click(function(e) {
         e.preventDefault();
         var dialog = $(e.currentTarget).attr('data-dialog');
 
         $('#dialog-'+dialog).toggleClass('active');
     });
+}
+
+// Setup audio player
+function setupAudioPlayers() {
+    $('.audio-player').each(function() {
+        var $player = $(this);
+        var audio = $player.find('audio').get(0);
+        var $position = $player.find('.position');
+        var loaded = false;
+
+        audio.onplaying = function() {
+            $player.addClass('playing');
+            $player.removeClass('paused');
+        };
+        audio.onpause = function() {
+            $player.addClass('playing');
+            $player.addClass('paused');
+        };
+        audio.onended = function() {
+            $player.removeClass('playing');
+            $player.removeClass('paused');
+        };
+        audio.ontimeupdate = function() {
+            var t = audio.currentTime;
+            var msg = "";
+
+            msg = Math.floor(t/60)+':'+Math.ceil(t % 60);
+
+            $position.text(msg);
+        };
+
+        $player.find('.do-play').click(function(e) {
+            e.preventDefault();
+            if (!loaded) {
+                audio.load();
+                loaded = true;
+            }
+            audio.play();
+        });
+
+        $player.find('.do-pause').click(function(e) {
+            e.preventDefault();
+            audio.pause();
+        });
+
+        $player.find('.do-stop').click(function(e) {
+            e.preventDefault();
+            audio.pause();
+            audio.currentTime = 0;
+        });
+    });
+}
+
+
+$(document).ready(function() {
+    setupDialogs();
+    setupAudioPlayers();
 
     // Call dialog
     setupDialDialog();
