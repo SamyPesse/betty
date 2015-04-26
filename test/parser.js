0b2eddb5a539d6119e30414cfd3626d9a3648f6c
Samy Pesse <samypesse@gmail.com>
2015-04-25 19:09:16 -0500

    Improve message parser

--- test/parser.js
+++ test/parser.js
@@ -1,10 +1,23 @@
 var expect = require('chai').expect;
-var parser = require('../lib/parser');
+var parser = require('../lib/utils/parser');
 
 describe('Messages Parser', function() {
 
+    it('should correctly reject non betty message', function() {
+        var r = parser.parse("Random message");
+
+        expect(r).to.not.be.ok;
+    });
+
+    it('should correctly accept betty message', function() {
+        var r = parser.parse("betty: Random message");
+
+        expect(r).to.be.ok;
+        expect(r.type).to.equal("unknown");
+    });
+
     it('should correctly parse a call action', function() {
-        var r = parser.parse("Call +140145170479 for me");
+        var r = parser.parse("betty: Call +140145170479");
 
         expect(r).to.be.ok;
         expect(r.type).to.equal("call");
